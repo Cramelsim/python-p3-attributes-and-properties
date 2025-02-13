8034448196848552865d84ac0defc0ff16548922
MELODY SIMWA <simwamelody@gmail.com>
2025-02-13 17:07:04 +0300

    update code

--- lib/dog.py
+++ lib/dog.py
@@ -2,27 +2,29 @@ class Dog:
     APPROVED_BREEDS = ["Labrador", "Poodle", "Bulldog", "Beagle", "Pug", "Golden Retriever"]
 
     def __init__(self, name="Buddy", breed="Labrador"):
-        self.name = name  # Calls the setter
-        self.breed = breed  # Calls the setter
+        self.name = name  
+        self.breed = breed  
 
-    @property
-    def name(self):
+    def get_name(self):
+        print("Getting name")
         return self._name
 
-    @name.setter
-    def name(self, value):
-        if isinstance(value, str) and 1 <= len(value) <= 25:
-            self._name = value
-        else:
+    def set_name(self, value):
+        if not isinstance(value, str) or not (1 <= len(value) <= 25):
             print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = value.title()
 
-    @property
-    def breed(self):
+    def get_breed(self):
+        print("Getting breed")
         return self._breed
 
-    @breed.setter
-    def breed(self, value):
-        if value in self.APPROVED_BREEDS:
-            self._breed = value
-        else:
+    def set_breed(self, value):
+        if value not in self.APPROVED_BREEDS:
             print("Breed must be in list of approved breeds.")
+        else:
+            self._breed = value
+
+    
+    name = property(get_name, set_name)
+    breed = property(get_breed, set_breed)
