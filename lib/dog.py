a1293a1c6a02b694bda0584df0bb1504959b68eb
MELODY SIMWA <simwamelody@gmail.com>
2025-02-13 20:12:11 +0300

    update

--- lib/dog.py
+++ lib/dog.py
@@ -9,21 +9,21 @@ class Dog:
         print("Getting name")
         return self._name
 
-    def set_name(self, value):
-        if not isinstance(value, str) or not (1 <= len(value) <= 25):
+    def set_name(self, name):
+        if not isinstance(name, str) or not (1 <= len(name) <= 25):
             print("Name must be string between 1 and 25 characters.")
         else:
-            self._name = value.title()
+            self._name = name.title()
 
     def get_breed(self):
         print("Getting breed")
         return self._breed
 
-    def set_breed(self, value):
-        if value not in self.APPROVED_BREEDS:
+    def set_breed(self, breed):
+        if breed not in self.APPROVED_BREEDS:
             print("Breed must be in list of approved breeds.")
         else:
-            self._breed = value
+            self._breed = breed
 
     
     name = property(get_name, set_name)
